71344e0db97b5b171657b527657080e7f5559874
Nicolas Herbaut <nicolas.herbaut@gmail.Com>
2020-07-05 09:38:06 +0000

    vue poc conso-api

--- vue-keycloak/src/main.js
+++ vue-keycloak/src/main.js
@@ -19,7 +19,7 @@ Vue.use(VueLogger, options);
 
 //keycloak init options
 let initOptions = {
-  url: 'http://127.0.0.1:8081/auth', realm: 'keycloak-demo', clientId: 'vue-test-app', onLoad:'login-required'
+  url: 'https://auth.vod-prime.space/auth', realm: 'discoverability', clientId: 'vue-test-app', onLoad:'login-required'
 }
 
 let keycloak = Keycloak(initOptions);
